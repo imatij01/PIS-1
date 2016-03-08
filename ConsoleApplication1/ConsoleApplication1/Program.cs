a825b7c7163ec9b34ea4b487879573faaf7df410
ssinov00 <sinovcicstela@gmail.com>
2016-03-08 10:21:40 +0100

    dodano!

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,7 +9,9 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello World");
+            Console.WriteLine("Nemas pojma!!!");
         }
     }
 }
